fcd23cd3f79c4936d4a5e8bc1881a189c4e5903e
Shihao Xia <charlesxsh@hotmail.com>
2022-06-07 14:21:14 -0700

    update zeus controllers

--- src/generated/controllers.ts
+++ src/generated/controllers.ts
@@ -1,140 +1,117 @@
-import {
-  ApiTags,
-  ApiPropertyOptional,
-  ApiProperty,
-  ApiOkResponse,
-  ApiBearerAuth,
-} from "@nestjs/swagger";
-import {
-  Inject,
-  Controller,
-  Get,
-  Post,
-  Delete,
-  Put,
-  Param,
-  Query,
-  Body,
-  ParseIntPipe,
-  ParseBoolPipe,
-} from "@nestjs/common";
-import {
-  GeneralObjectStatus,
-  GeneralObjectMeta,
-  DaoService,
-  DAO_SERVICE,
-  ListObjectsRequest,
-  UpdateObjectRequest,
-  DeleteObjectRequest,
-} from "./types";
+import { ApiTags, ApiPropertyOptional, ApiProperty, ApiOkResponse, ApiBearerAuth, ApiQuery } from "@nestjs/swagger";
+import { Inject, Controller, Get, Post, Delete, Put, Param, Query, Body, ParseIntPipe, ParseBoolPipe } from "@nestjs/common";
+import { GeneralObjectStatus, GeneralObjectMeta, DaoService, DAO_SERVICE, ListObjectsRequest, UpdateObjectRequest, DeleteObjectRequest } from "./types";
 export class GeneralObjectMetaDto {
-  @ApiPropertyOptional()
-  id?: string;
-  @ApiPropertyOptional()
-  userId?: string;
-  @ApiPropertyOptional()
-  typeId?: string;
-  @ApiPropertyOptional({ enum: GeneralObjectStatus })
-  status?: string;
-  @ApiPropertyOptional()
-  updatedAt?: number;
-  @ApiPropertyOptional()
-  createdAt?: number;
-  static fromRaw(raw: GeneralObjectMeta): GeneralObjectMetaDto {
-    if (!raw) {
-      return raw as any;
+    @ApiPropertyOptional()
+    id?: string;
+    @ApiPropertyOptional()
+    userId?: string;
+    @ApiPropertyOptional()
+    typeId?: string;
+    @ApiPropertyOptional({ enum: GeneralObjectStatus })
+    status?: string;
+    @ApiPropertyOptional()
+    updatedAt?: number;
+    @ApiPropertyOptional()
+    createdAt?: number;
+    static fromRaw(raw: GeneralObjectMeta): GeneralObjectMetaDto {
+        if (!raw) {
+            return raw as any;
+        }
+        return {
+            id: raw.id,
+            userId: raw.userId,
+            typeId: raw.typeId,
+            status: GeneralObjectStatus[raw.status],
+            updatedAt: raw.updatedAt,
+            createdAt: raw.createdAt
+        };
     }
-    return {
-      id: raw.id,
-      userId: raw.userId,
-      typeId: raw.typeId,
-      status: GeneralObjectStatus[raw.status],
-      updatedAt: raw.updatedAt,
-      createdAt: raw.createdAt,
-    };
-  }
 }
 export class ListObjectsResponseDto {
-  @ApiPropertyOptional()
-  totalCount?: number;
-  @ApiPropertyOptional()
-  nextToken?: string;
-  @ApiPropertyOptional({ type: [String] })
-  items?: string[];
+    @ApiPropertyOptional()
+    totalCount?: number;
+    @ApiPropertyOptional()
+    nextToken?: string;
+    @ApiPropertyOptional({ type: [String] })
+    items?: string[];
 }
 export class UpdateObjectResponseDto {
-  @ApiPropertyOptional()
-  updatedObject?: string;
+    @ApiPropertyOptional()
+    updatedObject?: string;
 }
 export class UpdateObjectRequestBodyDto {
-  @ApiPropertyOptional()
-  updatedObject?: string;
+    @ApiPropertyOptional()
+    updatedObject?: string;
 }
 export class DeleteObjectResponseDto {
-  @ApiPropertyOptional()
-  success?: boolean;
+    @ApiPropertyOptional()
+    success?: boolean;
 }
-export class DeleteObjectRequestBodyDto {}
+export class DeleteObjectRequestBodyDto {
+}
 @Controller("DaoService")
 export class DaoServiceController {
-  constructor(
+    constructor(
     @Inject(DAO_SERVICE)
-    private readonly daoService: DaoService
-  ) {}
-  @Get("gaea/objects")
-  @ApiOkResponse({ type: ListObjectsResponseDto })
-  async listObjects(
+    private readonly daoService: DaoService) { }
+    @Get("gaea/objects")
+    @ApiOkResponse({ type: ListObjectsResponseDto })
+    @ApiQuery({ name: "id", required: false })
+    @ApiQuery({ name: "userId", required: false })
+    @ApiQuery({ name: "typeId", required: false })
+    @ApiQuery({ name: "status", required: false, enum: GeneralObjectStatus })
+    @ApiQuery({ name: "limit", required: false })
+    @ApiQuery({ name: "nextToken", required: false })
+    async listObjects(
     @Query("id")
-    id: string,
+    id: string, 
     @Query("userId")
-    userId: string,
+    userId: string, 
     @Query("typeId")
-    typeId: string,
+    typeId: string, 
     @Query("status")
-    status: string,
+    status: string, 
     @Query("limit", ParseIntPipe)
-    limit: number,
+    limit: number, 
     @Query("nextToken")
-    nextToken: string
-  ): Promise<ListObjectsResponseDto> {
-    const _req = {
-      id,
-      userId,
-      typeId,
-      status: GeneralObjectStatus[status],
-      limit,
-      nextToken,
-    } as ListObjectsRequest;
-    const _res = await this.daoService.listObjects(_req);
-    return _res;
-  }
-  @Put("gaea/object/:id")
-  @ApiOkResponse({ type: UpdateObjectResponseDto })
-  async updateObject(
+    nextToken: string): Promise<ListObjectsResponseDto> {
+        const _req = {
+            id,
+            userId,
+            typeId,
+            status: GeneralObjectStatus[status],
+            limit,
+            nextToken
+        } as ListObjectsRequest;
+        const _res = await this.daoService.listObjects(_req);
+        return _res;
+    }
+    @Put("gaea/object/:id")
+    @ApiOkResponse({ type: UpdateObjectResponseDto })
+    async updateObject(
     @Param("id")
-    id: string,
+    id: string, 
     @Body()
-    body: UpdateObjectRequestBodyDto
-  ): Promise<UpdateObjectResponseDto> {
-    const _req = {
-      id,
-      updatedObject: body.updatedObject,
-    } as UpdateObjectRequest;
-    const _res = await this.daoService.updateObject(_req);
-    return _res;
-  }
-  @Delete("gaea/object/:id")
-  @ApiOkResponse({ type: DeleteObjectResponseDto })
-  async deleteObject(
+    body: UpdateObjectRequestBodyDto): Promise<UpdateObjectResponseDto> {
+        const _req = {
+            id,
+            updatedObject: body.updatedObject
+        } as UpdateObjectRequest;
+        const _res = await this.daoService.updateObject(_req);
+        return _res;
+    }
+    @Delete("gaea/object/:id")
+    @ApiOkResponse({ type: DeleteObjectResponseDto })
+    async deleteObject(
     @Param("id")
-    id: string,
+    id: string, 
     @Body()
-    body: DeleteObjectRequestBodyDto
-  ): Promise<DeleteObjectResponseDto> {
-    const _req = {
-      id,
-    } as DeleteObjectRequest;
-    const _res = await this.daoService.deleteObject(_req);
-    return _res;
-  }
+    body: DeleteObjectRequestBodyDto): Promise<DeleteObjectResponseDto> {
+        const _req = {
+            id
+        } as DeleteObjectRequest;
+        const _res = await this.daoService.deleteObject(_req);
+        return _res;
+    }
 }
--- src/generated/types.ts
+++ src/generated/types.ts
@@ -1,44 +1,44 @@
 export interface ListObjectsRequest {
-  id?: string;
-  userId?: string;
-  typeId?: string;
-  status?: GeneralObjectStatus;
-  limit?: number;
-  nextToken?: string;
+    id?: string;
+    userId?: string;
+    typeId?: string;
+    status?: GeneralObjectStatus;
+    limit?: number;
+    nextToken?: string;
 }
 export interface ListObjectsResponse {
-  totalCount?: number;
-  nextToken?: string;
-  items?: string[];
+    totalCount?: number;
+    nextToken?: string;
+    items?: string[];
 }
 export interface UpdateObjectRequest {
-  id?: string;
-  updatedObject?: string;
+    id?: string;
+    updatedObject?: string;
 }
 export interface UpdateObjectResponse {
-  updatedObject?: string;
+    updatedObject?: string;
 }
 export interface DeleteObjectRequest {
-  id?: string;
+    id?: string;
 }
 export interface DeleteObjectResponse {
-  success?: boolean;
+    success?: boolean;
 }
 export interface DaoService {
-  listObjects(request: ListObjectsRequest): Promise<ListObjectsResponse>;
-  updateObject(request: UpdateObjectRequest): Promise<UpdateObjectResponse>;
-  deleteObject(request: DeleteObjectRequest): Promise<DeleteObjectResponse>;
+    listObjects(request: ListObjectsRequest): Promise<ListObjectsResponse>;
+    updateObject(request: UpdateObjectRequest): Promise<UpdateObjectResponse>;
+    deleteObject(request: DeleteObjectRequest): Promise<DeleteObjectResponse>;
 }
 export const DAO_SERVICE = Symbol("DAO_SERVICE");
 export enum GeneralObjectStatus {
-  Active = 1,
-  Deleted = 2,
+    Active = 1,
+    Deleted = 2
 }
 export interface GeneralObjectMeta {
-  id?: string;
-  userId?: string;
-  typeId?: string;
-  status?: GeneralObjectStatus;
-  updatedAt?: number;
-  createdAt?: number;
+    id?: string;
+    userId?: string;
+    typeId?: string;
+    status?: GeneralObjectStatus;
+    updatedAt?: number;
+    createdAt?: number;
 }
