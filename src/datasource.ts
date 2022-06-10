a6bc11bcbf838c2364795d243b62482f29635f62
Shihao Xia <charlesxsh@hotmail.com>
2022-06-10 01:11:32 -0400

    make migration as part as src

--- src/datasource.ts
+++ src/datasource.ts
@@ -3,6 +3,7 @@ import { DataSource } from "typeorm";
 const AppDataSource = new DataSource({
   type: process.env.TYPEORM_CONNECTION as any,
   url: process.env.TYPEORM_URL,
+  migrations: ((process.env.TYPEORM_MIGRATIONS as any) || "").split(","),
   logging: false,
 });
 
